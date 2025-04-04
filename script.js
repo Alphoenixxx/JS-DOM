ac0e4356a7bf6ba84c89b1fc786e9835c223308e
Onyx <mersia.cepeljac@outlook.be>
2025-04-04 14:51:19 +0200

    Etape 2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const input = document.getElementById("task-input");
 const button = document.getElementById("add-task");
 const list = document.getElementById("task-list");
 
+//Étape 2
+button.addEventListener("click", function () {
+    // Code à exécuter quand on clique sur le bouton
+  });
