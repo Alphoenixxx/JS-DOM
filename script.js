ec9019e7bb0c87dc8673d2ccd7505cc16f168859
Onyx <mersia.cepeljac@outlook.be>
2025-04-04 15:00:48 +0200

    Etape 4

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Étape 1
+// Étape 2
 const input = document.getElementById("task-input");
 const button = document.getElementById("add-task");
 const list = document.getElementById("task-list");
 
-//Étape 2
+//Étape 3
 button.addEventListener("click", function () {
     // Code à exécuter quand on clique sur le bouton
 
-    //Etape 3
     const taskText = input.value;
+
+    if (taskText.trim() === "") {
+        return; // ne fait rien si le champ est vide
+      }
+
+    const newTask = document.createElement("li");
+    newTask.textContent = taskText;
+    list.appendChild(newTask);
+    input.value = "";
+
+    // Étape 4
+    newTask.addEventListener('click', function() {
+        this.remove();
+    });
   });
